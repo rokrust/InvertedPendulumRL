1b608ba6e09ad40b15995790a3696b6acfe9761a
rokrust <rokrust@hotmail.com>
2017-07-03 17:39:05 +0200

    do more stuff

--- src/testTensor.py
+++ src/testTensor.py
@@ -5,6 +5,8 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
 import tensorflow as tf
+import random
+
 
 # Parameters
 learning_rate = 0.001
@@ -28,15 +30,18 @@ def multilayer_perceptron(x, weights, biases):
     # Hidden layer with RELU activation
     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
     layer_1 = tf.nn.relu(layer_1)
+
     # Hidden layer with RELU activation
     layer_2 = tf.matmul(layer_1, weights['h2']) + biases['b2']
     layer_2 = tf.nn.relu(layer_2)
 
     layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])
     layer_3 = tf.nn.relu(layer_3)
+
     # Output layer with linear activation
     out_layer = tf.matmul(layer_3, weights['out']) + biases['out']
     return out_layer
+
 
 # Store layers weight & bias
 weights = {
@@ -45,6 +50,7 @@ weights = {
     'h3': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
     'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
 }
+
 biases = {
     'b1': tf.Variable(tf.random_normal([n_hidden_1])),
     'b2': tf.Variable(tf.random_normal([n_hidden_2])),
@@ -53,7 +59,7 @@ biases = {
 }
 
 # Construct model
-pred = multilayer_perceptron(x, weights, biases)
+network = multilayer_perceptron(x, weights, biases)
 
 # Define loss and optimizer
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
@@ -66,10 +72,67 @@ init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
 
+	n_runs = 1000
+	
+	#hyper parameters
+	epsilon = 1
+	learning_rate = 0.001
+	gamma = 0.01
+
+	#state variables	
+	current_state = None
+	next_state = env.reset() #init model
+	reward = 0 #Final reward
+	done = 0  #Termination condition
+
+	D = []
+	for range(n_runs):
+
+		while not done:
+			current_state = next_state
+			Q_l, Q_r = sess.run(network, feed_dict={x : current_state})
+
+			if random.random() < epsilon:
+				action = randint(0, 2)
+
+			elif Q_l > Q_r:
+				Q = Q_l
+				action = 0
+
+			else:
+				Q = Q_r
+				action = 1
+			
+			#Step through model
+			next_state, current_reward, done, info = env.step(action)
+			reward += current_reward
+			
+			#Save transitions
+			D.append((current_state, Q, action, next_state))
+	
+		shuffle(D) #Pick a random transition from D
+		for transitions in D
+			s, r, a, s_n = transitions
+
+			#Calculate error and train network
+			Q_n = sess.run(network, feed_dict={x : s_n})
+			Q_target = r + gamma*max(sess.run(network, feed_dict={x : s_n})
+					
+			Q = sess.run(network, feed_dict={x : s}[a] #Get new reward in the updated network
+			network.train(Q_target - Q)
+
+		del D[:]
+
+	sess.run(network, feed_dict={x : s_n})
+	
+
+#.............................Not used...................................#
+"""
     # Training cycle
     for epoch in range(training_epochs):
-        avg_cost = 0.
+        avg_cost = 0.	
         total_batch = int(mnist.train.num_examples/batch_size)
+
         # Loop over all batches
         for i in range(total_batch):
             batch_x, batch_y = mnist.train.next_batch(batch_size)
@@ -78,6 +141,7 @@ with tf.Session() as sess:
                                                           y: batch_y})
             # Compute average loss
             avg_cost += c / total_batch
+
         # Display logs per epoch step
         if epoch % display_step == 0:
             print("Epoch:", '%04d' % (epoch+1), "cost=", \
@@ -86,6 +150,9 @@ with tf.Session() as sess:
 
     # Test model
     correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+
     # Calculate accuracy
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
     print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+"""
+#.........................................................................#
